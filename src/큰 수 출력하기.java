e10d7a51cf3ab8f44935f67b455966c29f60887c
yoori choi <vndn6832@likelion.org>
2021-09-17 11:15:17 +0900

    큰 수 출력하기 > nextInt(); > 배열 선언, 다루기 > Integer vs int

---
+++ src/큰 수 출력하기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Main2and1{
+
+    public ArrayList<Integer> solution(int n, int[] arr){
+        ArrayList<Integer> ans = new ArrayList<>();
+        ans.add(arr[0]);
+        for(int i=1;i<n;i++){
+            if(arr[i]>arr[i-1])
+                ans.add(arr[i]);
+        }
+
+        return ans;
+    }
+
+
+    public static void main(String[] args){
+        Main2and1 T = new Main2and1();
+        Scanner S = new Scanner(System.in);
+
+        int N = S.nextInt();
+
+        int[] arr = new int[N];
+
+        for(int i=0;i<N;i++){
+            arr[i] = S.nextInt();
+        }
+
+        for(int x: T.solution(N,arr)){
+            System.out.print(x+ " ");
+        }
+
+        S.close();
+
+    }
+}
