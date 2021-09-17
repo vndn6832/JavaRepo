ea3808f4c45df872fe82b8e7a84eb29ddd2644da
yoori choi <vndn6832@likelion.org>
2021-09-17 20:08:28 +0900

    임시반장정하기 > i : 기준 학생 > j: 비교하는 학생 > k: 반 번호 > //*** 한번만 카운트 하면됨 - 한번이라도 같은 반이었던 사람 찾는거니깐

---
+++ src/임시반장정하기.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+import java.util.*;
+class Main2and11{
+
+    public int solution(int n, int[][] arr){
+
+        int max = 0;
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            int cnt = 0;
+            for(int j=1;j<=n;j++){
+                
+                for(int k=1;k<=5;k++){
+                    if(arr[i][k] == arr[j][k]){
+                         cnt++;
+                         break; //*** 한번만 카운트 하면됨 - 한번이라도 같은 반이었던 사람 찾는거니깐
+                    }
+                }
+                
+            }
+            if(cnt > max){
+                    max = cnt;
+                    ans = i;
+            }
+        }
+
+        return ans;
+    }
+
+
+
+
+    public static void main(String[] args){
+        Main2and11 T = new Main2and11();
+        Scanner S = new Scanner(System.in);
+
+        int n = S.nextInt();
+        int arr[][] = new int[n+1][n+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=5;j++){ //5학년 까지
+                arr[i][j] = S.nextInt();
+            }
+        }
+
+        System.out.print(T.solution(n,arr));
+        S.close();
+
+    }
+}
