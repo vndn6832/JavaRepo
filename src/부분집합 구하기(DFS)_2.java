2be6d847614b12765210b00cf7a8e1a23a62aa7e
yoori choi <vndn6832@likelion.org>
2021-09-22 14:27:38 +0900

    부분집합 구하기(DFS) - 깔끔한 버전 > static int[] ch를 체크배열+원소배열로 사용

---
+++ src/부분집합 구하기(DFS)_2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Main7and6and2 {
+	static int n;
+	static int[] ch;
+	public void DFS(int L){
+		if(L==n+1){
+			String tmp="";
+			for(int i=1; i<=n; i++){
+				if(ch[i]==1) tmp+=(i+" ");
+			}
+			if(tmp.length()>0) System.out.println(tmp);
+		}
+		else{
+			ch[L]=1;
+			DFS(L+1);
+			ch[L]=0;
+			DFS(L+1);
+		}
+	}
+
+	public static void main(String[] args){
+		Main7and6and2 T = new Main7and6and2();
+		n=3;
+		ch=new int[n+1];
+		T.DFS(1);
+	}	
+}
